af5be34bd432c9e6d4e7a83ef7b040d331211b20
Giovanni Mendoza <mendozagioo@gmail.com>
2016-10-03 20:43:41 -0500

    Menu fixed and made for mobile screens

--- app/assets/javascripts/components_interaction.js
+++ app/assets/javascripts/components_interaction.js
@@ -2,6 +2,11 @@
   'use strict';
 
   $(document).on('ready', function() {
+    // Principal navigation menu
+    $('#principal-menu .toggle').click(function() {
+      $('#principal-menu nav').slideToggle();
+    });
+
     // Get file and put the selected file name in button
     $('.inputfile > label').click(function() {
       $('.inputfile > input').trigger('click');
